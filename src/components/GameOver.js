89af627e4bf344eba9dd2351993fd141744f3c6d
taniachque <taniachque@gmail.com>
2024-10-23 04:53:28 -0400

    Adding emojis to gameover componente and centering content.

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const GameOver = ({ isWinner, selectedWord, resetGame }) => {
   return (
-    <div className='py-8'>
+    <div className='flex flex-col items-center justify-center py-2'>
       {isWinner ? (
-        <h2>¡Ganaste! La palabra era: {selectedWord}</h2>
+        <h2>🎉 ¡Atipanki! La palabra era: {selectedWord} 😊</h2>
       ) : (
-        <h2>¡Perdiste! La palabra era: {selectedWord}</h2>
+        <h2>😢 ¡Atipachikunki! La palabra era: {selectedWord} 😔</h2>
       )}
       <button onClick={resetGame}
       className='bg-green-500 text-white font-bold mt-6 py-2 px-4 rounded hover:bg-green-600 transition duration-500'>Reiniciar Juego</button>
