7dbaad9a30327d7dc1b18623564a0ee2a1e010e5
taniachque <taniachque@gmail.com>
2024-10-30 16:46:47 -0400

    Added images to game menu. Game data modified.

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NinoLeyendo from '../assets/images/ninoleyendo.png';
+import Anagrama from '../assets/images/anagrama.png';
+import AyudaSimi from '../assets/images/ayuda_simi.png';
+import SopadeLetras from '../assets/images/sopa_de_letras.png';
+import EmparejarPalabras from '../assets/images/emparejar_palabra.png';
 
 const GameMenu = () => {
   return (
@@ -8,7 +11,7 @@ const GameMenu = () => {
       <h1 className="text-3xl font-bold text-center">Bienvenidos a la sección de juegos</h1>
       <div className="grid grid-cols-2 gap-4 mt-8 mb-8">
         <div className="card h-80 p-12 bg-white border border-gray-300 rounded shadow-md flex flex-col items-center">
-          <img src={NinoLeyendo} alt="" className='mb-4 rounded h-32 w-auto'/>
+          <img src={Anagrama} alt="" className='mb-4 rounded h-32 w-auto'/>
           <h1>Anagrama - T’ikraspa pukllay</h1>
           <p className='text-gray-700 text-base'>descripcion</p>
           <Link 
@@ -16,7 +19,7 @@ const GameMenu = () => {
           className='mt-auto bg-green-500 text-white px-4 py--2 rounded hover:bg-green-600' type="">Pukllana!</Link>
         </div>
         <div className="card h-80 p-12 bg-white border border-gray-300 rounded shadow-md flex flex-col items-center">
-          <img src={NinoLeyendo} alt="" className='mb-4 rounded h-32 w-auto'/>
+          <img src={AyudaSimi} alt="" className='mb-4 rounded h-32 w-auto'/>
           <h1>Salva a Simi - Simiman yanapay!
           </h1>
           <p className='text-gray-700 text-base'>descripcion</p>
@@ -25,7 +28,7 @@ const GameMenu = () => {
           className='mt-auto bg-green-500 text-white px-4 py--2 rounded hover:bg-green-600' type="">Pukllana!</Link>
         </div>
         <div className="card h-80 p-12 bg-white border border-gray-300 rounded shadow-md flex flex-col items-center">
-          <img src={NinoLeyendo} alt="" className='mb-4 rounded h-32 w-auto'/>
+          <img src={SopadeLetras} alt="" className='mb-4 rounded h-32 w-auto'/>
           <h1>Sopa de letras   - Qillqa jilli</h1>
           <p className='text-gray-700 text-base'>descripcion</p>
           <Link 
@@ -33,7 +36,7 @@ const GameMenu = () => {
           className='mt-auto bg-green-500 text-white px-4 py--2 rounded hover:bg-green-600' type="">Pukllana!</Link>
         </div>
         <div className="card h-80 p-12 bg-white border border-gray-300 rounded shadow-md flex flex-col items-center">
-          <img src={NinoLeyendo} alt="" className='mb-4 rounded h-32 w-auto'/>
+          <img src={EmparejarPalabras} alt="" className='mb-4 rounded h-32 w-auto'/>
           <h1>Emparejamiento  - Tinkuchispa pukllay</h1>
           <p className='text-gray-700 text-base'>descripcion</p>
           <Link 
--- src/data/gameData.js
+++ src/data/gameData.js
@@ -4,7 +4,14 @@ export const gameData = {
     tema1: [
       { id: 1, word: "Juk", image: require('../assets/images/1.png') },
       { id: 2, word: "Iskay", image: require('../assets/images/2.png') },
-      { id: 3, word: "Kimsa", image: require('../assets/images/3.png') }
+      { id: 3, word: "Kimsa", image: require('../assets/images/3.png') },
+      { id: 4, word: "Tawa", image: require('../assets/images/4.png') },
+      { id: 5, word: "Pichqa", image: require('../assets/images/5.png') },
+      { id: 6, word: "Suqta", image: require('../assets/images/6.png') },
+      { id: 7, word: "Qanchis", image: require('../assets/images/7.png') },
+      { id: 8, word: "Pusaq", image: require('../assets/images/8.png') },
+      { id: 9, word: "Chunka", image: require('../assets/images/9.png') },
+      { id: 10, word: "Jisq’un", image: require('../assets/images/10.png') }
     ],
     tema2: [
       { id: 4, word: "Tawa", image: require('../assets/images/4.png') },
